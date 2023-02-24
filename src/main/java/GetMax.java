c8523d3c575fef9cbc38185827572395d336b40f
Taite Jernigan <taite5100@gmail.com>
2023-02-24 20:41:21 +0000

    Completed Challenge Get Max

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,12 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int highest = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > highest) {
+                highest = arr[i];
+            }
+        }
+        return highest;
     }
 }
